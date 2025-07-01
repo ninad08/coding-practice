fee3cf782396ee1ebab29ff44a2baf25df6e6b83
ninad08 <105038329+ninad08@users.noreply.github.com>
2025-07-01 14:43:32 +0530

    Add files via upload
    Star pattern problems

---
+++ StarPattern/butterfly.java
@@ -0,0 +1,56 @@
+//        *        *
+//        **      **
+//        ***    ***
+//        ****  ****
+//        **********
+//        ****  ****
+//        ***    ***
+//        **      **
+//        *        *
+
+package StarPattern;
+
+public class butterfly {
+
+    public static void main(String[] args) {
+        for (int i = 1; i <=5 ; i++) {
+            for (int j = 1; j <= i ; j++) {
+                System.out.print("*");
+            }
+
+            for (int j = i; j < 5 ; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = i; j < 5 ; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i ; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+        for (int i = 2; i <= 5 ; i++) {
+
+            for (int j = i; j <= 5; j++) {
+                System.out.print("*");
+            }
+
+            for (int j = 1; j < i ; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j < i ; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = i; j <= 5 ; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+}
